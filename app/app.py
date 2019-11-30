cc2fda77604321917062c62aa5122b54fb1daeaf
imsrv01 <39862925+imsrv01@users.noreply.github.com>
2019-11-30 16:57:05 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def hello():
     return render_template("home.html")
 
+
+@app.route('/status')
+def hello():
+    return "{'status' : 'success'}"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)), debug=True)
